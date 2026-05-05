08baa7551b42ef051510342fa79aebb66bd015ed
rafstak-afk <rafstak@gmail.com>
2026-05-05 10:47:41 +0200

    Update train.js

--- functions/api/train.js
+++ functions/api/train.js
@@ -1,18 +1,8 @@
 export async function onRequestGet(context) {
-  const url = new URL(context.request.url);
-  const trainNumber = url.searchParams.get("trainNumber") || "3812";
-  const date = url.searchParams.get("date") || "2026-05-05";
-
-  const endpoint = `TU_WSTAW_ENDPOINT_PLK?trainNumber=${encodeURIComponent(trainNumber)}&date=${encodeURIComponent(date)}`;
-
-  const resp = await fetch(endpoint, {
-    headers: {
-      "X-API-Key": context.env.PLK_API_KEY
-    }
-  });
-
-  return new Response(await resp.text(), {
-    status: resp.status,
+  return new Response(JSON.stringify({
+    ok: true,
+    hasKey: !!context.env.PLK_API_KEY
+  }), {
     headers: {
       "content-type": "application/json; charset=utf-8"
     }
