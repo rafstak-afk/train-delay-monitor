2b8a69ce6f7b7bceeb1897f029b8a74da29355ff
rafstak-afk <rafstak@gmail.com>
2026-07-15 10:59:45 +0200

    Add API endpoint for train details retrieval

---
+++ functions/api/train-details.js
@@ -0,0 +1,49 @@
+export async function onRequestGet(context) {
+  const url = new URL(context.request.url);
+
+  const train =
+    url.searchParams.get("train") || "";
+
+  if (!train) {
+    return new Response(
+      JSON.stringify(
+        {
+          error: "Brak parametru train"
+        },
+        null,
+        2
+      ),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
+
+  return new Response(
+    JSON.stringify(
+      {
+        train,
+
+        status: "TODO",
+
+        lastConfirmedStation: null,
+        lastConfirmedTime: null,
+
+        route: []
+      },
+      null,
+      2
+    ),
+    {
+      headers: {
+        "Content-Type":
+          "application/json; charset=utf-8",
+        "Cache-Control":
+          "no-store"
+      }
+    }
+  );
+}
